a76efea35acfd1997243c28a5966b303cd3b38be
Gaspar Menesses <menessesgaspar@gmail.com>
2026-06-10 21:25:06 -0300

    db connector

---
+++ db_conn/conn.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+import mysql.connector
+
+load_dotenv()
+
+conexion = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+print("Conectado correctamente")
+
+cursor = conexion.cursor()
+cursor.execute("SELECT * FROM actividad")
+
+for fila in cursor:
+    print(fila)
+
+conexion.close()
