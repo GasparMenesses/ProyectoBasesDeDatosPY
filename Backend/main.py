ccaf8a9e5eb206c8c20685b8bfead8bffe44ec76
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-06-13 19:39:11 -0300

    Main + arranque front

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,25 @@
+# Importamos Flask para crear la aplicación web y render_template para mostrar archivos HTML
+from flask import Flask, render_template
+
+# Creamos la aplicación Flask
+app = Flask(__name__)
+
+# Ruta principal del sitio
+@app.route("/")
+def inicio():
+
+    # Muestra el archivo index.html
+    return render_template("index.html")
+
+# Verifica que este archivo sea el que se está ejecutando directamente y no importado desde otro módulo
+if __name__ == "__main__":
+
+    # Inicia el servidor Flask
+    # debug=True permite ver errores detallados y
+    # reiniciar automáticamente cuando guardamos cambios
+    app.run(debug=True)
+
+
 """from db_conn.conn import obtener_conexion
 
 try:
@@ -20,4 +42,5 @@ from flask import Flask, jsonify
 from asistencia_gestion import listar_asistencias, registrar_asistencia
 from disciplina gestion import listar_disciplinas, crear_disciplinas
 """
+
 
